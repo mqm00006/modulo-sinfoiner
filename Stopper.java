f5da6d9d2a10b8b4e8b953f702efa3a5132382c6
mario quesada <mqm00006@red.ujaen.es>
2015-05-11 19:03:48 +0200

    stopper

--- Stopper.java
+++ Stopper.java
@@ -5,29 +5,85 @@
  */
 package com.sinfonier.bolts;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author portatil
  */
 public class Stopper extends BaseSinfonierBolt {
 
+    private String stopperURL;
+    private String fieldToParse;
+    private Set<String> words=new HashSet<String>();
     public Stopper(String path) {
+        
+        
         super(path);
+        
     }
 
     @Override
     public void userprepare() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.stopperURL=this.getParam("textField");
+        this.fieldToParse=this.getParam("tagToParse");
+        try {
+            read();
+        } catch (IOException ex) {
+            Logger.getLogger(Stopper.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
     public void userexecute() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+        
+        String texto=(String)this.getField(fieldToParse);
+        
+        String aux[]=texto.split(" ");
+        String result="";
+        for(int i=0;i<aux.length;i++)
+        {
+            if(!words.contains(aux[i]))
+            {
+                
+                result+=aux[i]+" ";
+            }            
+        }
 
+        
+        this.addField("Stopper", result);
+        this.emit();
+        
+        
+    }
+    private void read() throws MalformedURLException, IOException
+    {
+        URL url= new URL(stopperURL);
+        URLConnection con=url.openConnection();
+        con.connect();
+        
+        BufferedReader file=new BufferedReader(new InputStreamReader(con.getInputStream()));
+        
+        String lin;
+        
+        while((lin=file.readLine())!=null)
+        {
+            words.add(lin.trim());
+        }
+        file.close();
+    }
     @Override
     public void usercleanup() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
     
 }
